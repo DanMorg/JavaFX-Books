cc3a2bb1a86def4ff05bbeab2f3fe80f75e5c8b5
Asus1 <Asus1@Asus>
2016-12-06 12:26:58 +0100

    intento 1

--- Books/src/ch/markery/books/view/BookEditDialogController.java
+++ Books/src/ch/markery/books/view/BookEditDialogController.java
@@ -1,157 +1,160 @@
 package ch.markery.books.view;
-
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 import org.controlsfx.dialog.Dialogs;
 
 import ch.markery.book.utils.DateUtil;
+import ch.markery.books.model.Book;
+
+
 
 /**
  * Dialog to edit details of a person.
  * 
- * @author Jose Luis Gracia Herrero y Daniel Morgado Jara
+ * @author jose luis 
  */
 public class BookEditDialogController {
-	
-	
 
-	@FXML
-	private TextField firstNameField;
-	@FXML
-	private TextField lastNameField;
-	@FXML
-	private TextField streetField;
-	@FXML
-	private TextField postalCodeField;
-	@FXML
-	private TextField cityField;
-	@FXML
-	private TextField birthdayField;
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField streetField;
+    @FXML
+    private TextField postalCodeField;
+    @FXML
+    private TextField cityField;
+    @FXML
+    private TextField birthdayField;
 
-	private Stage dialogStage;
-	private ch.markery.books.model.Book Book;
-	private boolean okClicked = false;
 
-	/**
-	 * Initializes the controller class. This method is automatically called
-	 * after the fxml file has been loaded.
-	 */
-	@FXML
-	private void initialize() {
-	}
+    private Stage dialogStage;
+    private Book person;
+    private boolean okClicked = false;
 
-	/**
-	 * Sets the stage of this dialog.
-	 * 
-	 * @param dialogStage
-	 */
-	public void setDialogStage(Stage dialogStage) {
-		this.dialogStage = dialogStage;
-	}
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    }
 
-	/**
-	 * Sets the person to be edited in the dialog.
-	 * 
-	 * 
-	 * @param person
-	 */
-	public void setBook(ch.markery.books.model.Book Book) {
-		this.Book = Book;
+    /**
+     * Sets the stage of this dialog.
+     * 
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
 
-		firstNameField.setText(Book.getTitle().toString());
-		lastNameField.setText(Book.getType().toString());
-		streetField.setText(Book.getAuthor().toString());
-		postalCodeField.setText(Book.getEditorial().toString());
-		cityField.setText(Book.getPages().toString());
-		birthdayField.setText(Book.getVersion().toString());
-		birthdayField.setPromptText("dd.mm.yyyy");      
-	}
+    /**
+     * Sets the person to be edited in the dialog.
+     * 
+     * @param person
+     */
+    public void setPerson(Book person) {
+        this.person = person;
 
-	/**
-	 * Returns true if the user clicked OK, false otherwise.
-	 * 
-	 * @return
-	 */
-	public boolean isOkClicked() {
-		return okClicked;
-	}
+        firstNameField.setText(person.getTitle().toString());
+        lastNameField.setText(person.getType().toString());
+        streetField.setText(person.getAuthor().toString());
+        postalCodeField.setText((person.getEditorial().toString()));
+        cityField.setText(person.getPages().toString());
+        birthdayField.setText((person.getVersion().toString()));
+        birthdayField.setPromptText("dd.mm.yyyy");
+    }
 
-	/**
-	 * Called when the user clicks ok.
-	 */
-	@FXML
-	private void handleOk() {
-		if (isInputValid()) {
-//			Book.setTitle(firstNameField.getText());
-//			Book.set(lastNameField.getText());
-//			Book.setStreet(streetField.getText());
-//			Book.setPostalCode(Integer.parseInt(postalCodeField.getText()));
-//			Book.setCity(cityField.getText());
-//			Book.setBirthday(DateUtil.parse(birthdayField.getText()));
+    /**
+     * Returns true if the user clicked OK, false otherwise.
+     * 
+     * @return
+     */
+    public boolean isOkClicked() {
+        return okClicked;
+    }
 
-			okClicked = true;
-			dialogStage.close();
-		}
-	}
+    /**
+     * Called when the user clicks ok.
+     */
+    @FXML
+    private void handleOk() {
+        if (isInputValid()) {
+            person.setFirstName(firstNameField.getText());
+            person.setLastName(lastNameField.getText());
+            person.setStreet(streetField.getText());
+            person.setPostalCode(Integer.parseInt(postalCodeField.getText()));
+            person.setCity(cityField.getText());
+            person.setBirthday(DateUtil.parse(birthdayField.getText()));
 
-	/**
-	 * Called when the user clicks cancel.
-	 */
-	@FXML
-	private void handleCancel() {
-		dialogStage.close();
-	}
+            okClicked = true;
+            dialogStage.close();
+        }
+    }
 
-	/**
-	 * Validates the user input in the text fields.
-	 * 
-	 * @return true if the input is valid
-	 */
-	private boolean isInputValid() {
-		String errorMessage = "";
+    /**
+     * Called when the user clicks cancel.
+     */
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
 
-		if (firstNameField.getText() == null || firstNameField.getText().length() == 0) {
-			errorMessage += "No valid first name!\n";
-		}
-		if (lastNameField.getText() == null || lastNameField.getText().length() == 0) {
-			errorMessage += "No valid last name!\n";
-		}
-		if (streetField.getText() == null || streetField.getText().length() == 0) {
-			errorMessage += "No valid street!\n";
-		}
+    /**
+     * Validates the user input in the text fields.
+     * 
+     * @return true if the input is valid
+     */
+    private boolean isInputValid() {
+        String errorMessage = "";
 
-		if (postalCodeField.getText() == null || postalCodeField.getText().length() == 0) {
-			errorMessage += "No valid postal code!\n";
-		} else {
-			// try to parse the postal code into an int.
-			try {
-				Integer.parseInt(postalCodeField.getText());
-			} catch (NumberFormatException e) {
-				errorMessage += "No valid postal code (must be an integer)!\n";
-			}
-		}
+        if (firstNameField.getText() == null || firstNameField.getText().length() == 0) {
+            errorMessage += "No valid first name!\n"; 
+        }
+        if (lastNameField.getText() == null || lastNameField.getText().length() == 0) {
+            errorMessage += "No valid last name!\n"; 
+        }
+        if (streetField.getText() == null || streetField.getText().length() == 0) {
+            errorMessage += "No valid street!\n"; 
+        }
 
-		if (cityField.getText() == null || cityField.getText().length() == 0) {
-			errorMessage += "No valid city!\n";
-		}
+        if (postalCodeField.getText() == null || postalCodeField.getText().length() == 0) {
+            errorMessage += "No valid postal code!\n"; 
+        } else {
+            // try to parse the postal code into an int.
+            try {
+                Integer.parseInt(postalCodeField.getText());
+            } catch (NumberFormatException e) {
+                errorMessage += "No valid postal code (must be an integer)!\n"; 
+            }
+        }
 
-		if (birthdayField.getText() == null || birthdayField.getText().length() == 0) {
-			errorMessage += "No valid birthday!\n";
-		} else {
-			if (!DateUtil.validDate(birthdayField.getText())) {
-				errorMessage += "No valid birthday. Use the format dd.mm.yyyy!\n";
-			}
-		}
+        if (cityField.getText() == null || cityField.getText().length() == 0) {
+            errorMessage += "No valid city!\n"; 
+        }
 
-		if (errorMessage.length() == 0) {
-			return true;
-		} else {
-			// Show the error message.
-			Dialogs.create().title("Invalid Fields").masthead("Please correct invalid fields").message(errorMessage)
-					.showError();
-			return false;
-		}
-	}
+        if (birthdayField.getText() == null || birthdayField.getText().length() == 0) {
+            errorMessage += "No valid birthday!\n";
+        } else {
+            if (!DateUtil.validDate(birthdayField.getText())) {
+                errorMessage += "No valid birthday. Use the format dd.mm.yyyy!\n";
+            }
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Show the error message.
+            Dialogs.create()
+                .title("Invalid Fields")
+                .masthead("Please correct invalid fields")
+                .message(errorMessage)
+                .showError();
+            return false;
+        }
+    }
 }
